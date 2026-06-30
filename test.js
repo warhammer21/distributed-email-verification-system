e4882d3c8b29c0fd3f482d33033644770ba8c0bf
warhammer21 <shreyakvashisht21@gmail.com>
2026-07-01 03:05:16 +0530

    adding users rAMP UP

--- test.js
+++ test.js
@@ -23,3 +23,17 @@ export default function () {
     'status is success': (r) => r.status >= 200 && r.status < 300,
   });
 }
+export const options = {
+  // hosts line is completely removed
+  stages: [
+     { duration: '0s', target: 10 },  // Instantly jump to 10 users
+     { duration: '30s', target: 10 },
+  ],
+  thresholds: { http_req_duration: ['avg<100', 'p(95)<200'] },
+  noConnectionReuse: true,
+  userAgent: 'MyK6UserAgentString/1.0',
+};
+
+export default function () {
+  http.get('http://localhost:8081/request-registration');
+}
